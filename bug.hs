6df80cfaf248a29d162220af82b2937cc309141c
BugHunter-X <askzenai@gmail.com>
2024-12-26 01:24:05 +0100

    Add bug.hs file

---
+++ bug.hs
@@ -0,0 +1,12 @@
+{-# LANGUAGE FlexibleInstances #-}
+
+instance (Show a, Show b) => Show (a, b) where
+  show (a, b) = "(" ++ show a ++ "," ++ show b ++ ")"
+
+instance Show (Int, String) where
+  show (i, s) = "IntString (" ++ show i ++ "," ++ s ++ ")"
+
+main :: IO ()
+main = do
+  print (1, "hello")
+  print (1.0, 1.0)
